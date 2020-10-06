7632a307db5ac72f6bb2b8e05628b133108d54ba
Michael-Paul Cuccia <michaelpaulcuccia@yahoo.com>
2020-10-06 17:31:39 -0400

    additional parks, updated functions

--- flask_app/app.py
+++ flask_app/app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request
 import bs4, requests
 app = Flask(__name__)
 
-
 @app.route('/')
 def home_page():
     return render_template('home.html')
@@ -11,13 +10,17 @@ def home_page():
 @app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
+
+        #unique 4 letter park key for URL - source home.html, <select name="park_choice"></select>
         park_choice = request.form['park_choice']
         
+        #build URL Request
         pre_url = 'https://www.nps.gov/'
         park_var = park_choice
         suf_url = '/index.htm'
         url = pre_url + park_var + suf_url
 
+        #make URL request, get response code/status, entire page returns as 'soup'
         res = requests.get(url)
         print('Res Status Code: ' + str(res.status_code)) #200
         print('Any Issues: ' + str(res.raise_for_status())) #none
@@ -27,12 +30,14 @@ def submit():
         park_footer = soup.find('div', class_ = 'ParkFooter-contact')
         print('Scrape: ' + str(park_footer))
 
-        response_string = str(park_footer)
-
+        #stringify repsonse
+        response_string = str(park_footer)  
+        
         return render_template('success.html', response_data=response_string)
+
     else:
         
-        error_string = 'sorry, there was an error.'
+        error_string = 'sorry, there was a request error. request.method !== POST'
 
         return render_template('success.html', response_data=error_string)
 
--- flask_app/scripts/abbreviatedNames.js
+++
@@ -1,7 +0,0 @@
-export const abbreviatedNames = {
-    delaware_water_gap: 'dewa',
-    badlands: 'badl',
-    yosemite: 'yose',
-    wind_cave: 'wica',
-    mount_rushmore: 'moru'
-}
